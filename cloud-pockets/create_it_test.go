ec4d70d845582b61c07fa8ec59260ac6327f69cf
Ic3Sandy <monkey_king2@hotmail.com>
2023-01-14 15:05:09 +0700

    Add it test for create pocket

---
+++ cloud-pockets/create_it_test.go
@@ -0,0 +1,44 @@
+//go:build integration
+
+package cloud_pockets
+
+import (
+	"database/sql"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/kkgo-software-engineering/workshop/config"
+	"github.com/labstack/echo/v4"
+	_ "github.com/lib/pq"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreatePocketIT(t *testing.T) {
+	body := `{"name":"test-pocket", "currency":"THB", "balance":100.00}`
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodPost, "/cloud-pockets", strings.NewReader(body))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	req.SetBasicAuth("admin", "secret")
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	cfg := config.New().All()
+	sql, err := sql.Open("postgres", cfg.DBConnection)
+	if err != nil {
+		t.Error(err)
+	}
+	hCloudPocket := New(sql)
+
+	if assert.NoError(t, hCloudPocket.CreateCloudPocket(c)) {
+		assert.Equal(t, http.StatusCreated, rec.Code)
+		var response map[string]interface{}
+		json.Unmarshal(rec.Body.Bytes(), &response)
+		assert.Equal(t, "test-pocket", response["name"])
+		assert.Equal(t, "THB", response["currency"])
+		assert.Equal(t, 100.0, response["balance"])
+		assert.NotEmpty(t, response["id"])
+	}
+}
