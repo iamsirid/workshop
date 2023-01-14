e7b777ff742eb920680316bfb3fa519f1b5944c7
Ic3Sandy <monkey_king2@hotmail.com>
2023-01-14 14:07:31 +0700

    Update script init

--- db/01-init.sql
+++ db/01-init.sql
@@ -2,7 +2,7 @@ CREATE SEQUENCE IF NOT EXISTS account_id;
 
 CREATE TABLE
     "accounts" (
-        "id" int4 NOT NULL DEFAULT nextval('account_id':: regclass),
+        "id" int4 NOT NULL DEFAULT nextval('account_id' :: regclass),
         "balance" float8 NOT NULL DEFAULT 0,
         PRIMARY KEY ("id")
     );
@@ -13,5 +13,5 @@ CREATE TABLE
         name TEXT,
         category TEXT,
         currency TEXT,
-        balance float8 NOT NULL DEFAULT 0,
+        balance float8 NOT NULL DEFAULT 0
     );
