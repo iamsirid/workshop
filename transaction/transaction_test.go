6281c37d11a41853011c2e053f761beb2a8e0b17
NATTAPON <than_lnw@hotmail.com>
2023-01-15 10:33:24 +0700

    edit file Transaction_test.go

--- transaction/transaction_test.go
+++ transaction/transaction_test.go
@@ -3,120 +3,40 @@
 package transaction
 
 import (
-	"database/sql"
-	"errors"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
-	"github.com/kkgo-software-engineering/workshop/config"
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGetTransactionbyAccountid(t *testing.T) {
-	tests := []struct {
-		name       string
-		cfgFlag    config.FeatureFlag
-		sqlFn      func() (*sql.DB, error)
-		reqBody    string
-		wantStatus int
-		wantBody   string
-	}{
-		{"create account succesfully",
-			config.FeatureFlag{},
-			func() (*sql.DB, error) {
-				db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
-				if err != nil {
-					return nil, err
-				}
-				row := sqlmock.NewRows([]string{"id"}).AddRow(1)
-				mock.ExpectQuery(cStmt).WithArgs(1000.0).WillReturnRows(row)
-				return db, err
-			},
-			`{"balance": 1000.0}`,
-			http.StatusCreated,
-			`{"id": 1, "balance": 1000.0}`,
-		},
-		
+func TestTransactionbyAccountID(t *testing.T) {
+	//Arrange
+	req := httptest.NewRequest(http.MethodGet, "/cloud-pockets/1/transactions", strings.NewReader(""))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	newsMockRows := sqlmock.NewRows([]string{"id", "source_cloud_pocket_id", "destination_cloud_pocket_id", "description", "datetime","status"}).
+		AddRow(1,100,102,"101 to 102","2022-01-01 00:00:00","Success")
+	db, mock, err := sqlmock.New()
+	mock.ExpectPrepare("SELECT id,source_cloud_pocket_id,destination_cloud_pocket_id,amount,description,datetime,status FROM transaction WHERE id=1").ExpectQuery().WillReturnRows(newsMockRows)
+	// fmt.Println(newsMockRows)
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+	h := New(db)
+	c := echo.New().NewContext(req, rec)
+	expected := "[{\"id\":1,\"source_cloud_pocket_id\":\"100\",\"destination_cloud_pocket_id\":\"102\",\"description\":\"101 to 102\",\"datetime\":\"2022-01-01 00:00:00\",\"status\":Success}]"
+
+	//Act
+	err = h.GetAllCloudPocket(c)
+	//Arrange
+	if assert.NoError(t, err) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Equal(t, expected, strings.TrimSpace(rec.Body.String()))
 	}
 
-	for _, tc := range tests {
-		t.Run(tc.name, func(t *testing.T) {
-			e := echo.New()
-			req := httptest.NewRequest(http.MethodGet, "/",nil)
-			req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
-			rec := httptest.NewRecorder()
-			c := e.NewContext(req, rec)
-
-			db, err := tc.sqlFn()
-			h := New(tc.cfgFlag, db)
-			// Assertions
-			assert.NoError(t, err)
-			if assert.NoError(t, h.GetTransactionbyAccountid(c)) {
-				assert.Equal(t, tc.wantStatus, rec.Code)
-				assert.JSONEq(t, tc.wantBody, rec.Body.String())
-			}
-		})
-	}
-}
-
-func TestCreateAccount_Error(t *testing.T) {
-	someErr := errors.New("some random error")
-	tests := []struct {
-		name    string
-		cfgFlag config.FeatureFlag
-		sqlFn   func() (*sql.DB, error)
-		reqBody string
-		wantErr error
-	}{
-		{"create account failed",
-			config.FeatureFlag{},
-			func() (*sql.DB, error) {
-				db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
-				if err != nil {
-					return nil, err
-				}
-				mock.ExpectQuery(cStmt).WithArgs(1000.0).WillReturnError(someErr)
-				return db, err
-			},
-			`{"balance": 1000.0}`,
-			someErr,
-		},
-		{"create with bad request",
-			config.FeatureFlag{},
-			func() (*sql.DB, error) {
-				return nil, nil
-			},
-			`ba`,
-			echo.NewHTTPError(http.StatusBadRequest, "bad request body"),
-		},
-		{"create account balance exceed limitation and enable feature should failed",
-			config.FeatureFlag{IsLimitMaxBalanceOnCreate: true},
-			func() (*sql.DB, error) {
-				return nil, nil
-			},
-			`{"balance": 10000.1}`,
-			hErrBalanceLimitExceed,
-		},
-	}
-
-	for _, tc := range tests {
-		t.Run(tc.name, func(t *testing.T) {
-			e := echo.New()
-			req := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(tc.reqBody))
-			req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
-			rec := httptest.NewRecorder()
-			c := e.NewContext(req, rec)
-
-			db, _ := tc.sqlFn()
-			h := New(tc.cfgFlag, db)
-
-			berr := h.Create(c)
-			// Assertions
-			assert.Equal(t, berr, tc.wantErr)
-		})
-	}
 }
